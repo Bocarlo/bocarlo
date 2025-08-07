6eb71711e51b49cabdb1d4591983b3fee653fa3f
Bocarlo <bocarlo@o2.pl>
2025-08-07 04:51:08 +0200

    fix deploy path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bocarlo.github.io/bocarlo",
+  site: "https://bocarlo.github.io/bocarlo/",
   base: "/bocarlo/",
   integrations: [icon()],
   vite: {
